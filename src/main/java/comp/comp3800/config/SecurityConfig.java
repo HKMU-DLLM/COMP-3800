73cd29acb2e7c83c1fa9d9be0475808e658547b2
tskrin <tsukisaki.rin@gmail.com>
2026-04-12 00:16:23 +0800

    complete some security config

--- src/main/java/comp/comp3800/config/SecurityConfig.java
+++ src/main/java/comp/comp3800/config/SecurityConfig.java
@@ -17,11 +17,22 @@ public class SecurityConfig {
             throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        //work from up to down so 'all user can read' is  at bottom
+                        //just copy the link and paste for has role teacher
                         .requestMatchers("/admin/users/**").hasRole("TEACHER")
+
                         .requestMatchers("/lecture/create").hasRole("TEACHER")
-                        .requestMatchers("/lecture/coursematerial/*/delete").hasRole("TEACHER")
+
+
+                        .requestMatchers("/lecture/coursematerial/{id}/delete").hasRole("TEACHER")
                         .requestMatchers("/lecture/coursematerial/*/attachment/*/delete").hasRole("TEACHER")
+
+
                         .requestMatchers("/poll/admin/**").hasRole("TEACHER")
+
+                        .requestMatchers("/poll/**").hasAnyRole("TEACHER","STUDENT")
+                        .requestMatchers("/lecture/coursematerial/**").hasAnyRole("TEACHER","STUDENT")
+
                         .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
